914d2c0ba53d99f4f1bda0a8df56c6f89e37a889
cash <cashwugeek@icloud.com>
2024-09-22 21:49:17 +0800

    htmx click

---
+++ src/main/java/com/cashwu/javahtmx/JavaHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.cashwu.javahtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHtmxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHtmxApplication.class, args);
+    }
+
+}
