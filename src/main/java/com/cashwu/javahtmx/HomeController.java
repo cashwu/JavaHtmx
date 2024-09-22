d2c61a28a2570cfdacbbce7aa312c24e4c119c17
cash <cashwugeek@icloud.com>
2024-09-22 21:53:39 +0800

    htmx search

--- src/main/java/com/cashwu/javahtmx/HomeController.java
+++ src/main/java/com/cashwu/javahtmx/HomeController.java
@@ -6,14 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    static List<String> searchResults =
+            List.of("one", "two", "three", "four", "five");
 
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/search")
+    public String search(String q, Model model) {
+        var filtered = searchResults
+                .stream()
+                .filter(s -> s.startsWith(q.toLowerCase()))
+                .toList();
+        model.addAttribute("results", filtered);
+        return "search :: results";
     }
 
     @PostMapping("/clicked")
